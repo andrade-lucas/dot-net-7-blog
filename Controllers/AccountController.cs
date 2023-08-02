535b0c7f48b045a6983843460db19bad5a303983
Lucas Andrade <lucasilva108@gmail.com>
2023-08-02 14:32:44 -0300

    Post controller and configurations

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,9 +4,11 @@ using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
 using Blog.ViewModels.Accounts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
+using System.Text.RegularExpressions;
 
 namespace Blog.Controllers;
 
@@ -15,9 +17,9 @@ public class AccountController : ControllerBase
 {
     [HttpPost("v1/accounts")]
     public async Task<IActionResult> Post(
-        [FromBody]RegisterViewModel model,
-        [FromServices]EmailService emailService,
-        [FromServices]BlogDataContext context
+        [FromBody] RegisterViewModel model,
+        [FromServices] EmailService emailService,
+        [FromServices] BlogDataContext context
     )
     {
         if (!ModelState.IsValid)
@@ -46,10 +48,11 @@ public class AccountController : ControllerBase
                 "Seja bem-vindo!",
                 $"<p>Essa é a sua senha temporária: {password}</p>"
             );
-            
+
             return Ok(new ResultViewModel<dynamic>(new
             {
-                user = user.Email, password
+                user = user.Email,
+                password
             }));
         }
         catch (DbUpdateException ex)
@@ -61,12 +64,12 @@ public class AccountController : ControllerBase
             return StatusCode(500, new ResultViewModel<string>("Erro interno no servidor."));
         }
     }
-    
+
     [HttpPost("v1/accounts/login")]
     public async Task<IActionResult> Login(
-        [FromBody]LoginViewModel model,
-        [FromServices]BlogDataContext context,
-        [FromServices]TokenService tokenService
+        [FromBody] LoginViewModel model,
+        [FromServices] BlogDataContext context,
+        [FromServices] TokenService tokenService
     )
     {
         if (!ModelState.IsValid) return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
@@ -91,5 +94,43 @@ public class AccountController : ControllerBase
         {
             return StatusCode(500, new ResultViewModel<string>("Erro interno no servidor"));
         }
+    }
+
+    [Authorize]
+    [HttpPost("v1/accounts/upload-image")]
+    public async Task<IActionResult> UploadImage(
+        [FromBody] UploadImageViewModel model,
+        [FromServices] BlogDataContext context
+    )
+    {
+        var fileName = $"{Guid.NewGuid().ToString()}.jpg";
+        var data = new Regex(@"^data.image\/[a-z]+;base64,").Replace(model.Base64Image, "");
+        var bytes = Convert.FromBase64String(data);
+
+        try
+        {
+            await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResultViewModel<string>("Erro interno no servidor"));
+        }
+
+        var user = await context.Users.FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+
+        if (user == null) return NotFound(new ResultViewModel<User>("Usuário não encontrado"));
+
+        user.Image = $"https://localhost:5199/images/{fileName}";
+        try
+        {
+            context.Users.Update(user);
+            await context.SaveChangesAsync();
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(500, new ResultViewModel<string>("Erro interno no servidor"));
+        }
+
+        return Ok(new ResultViewModel<string>(user.Image));
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Text.Json.Serialization;
 using Blog;
 using Blog.Data;
 using Blog.Services;
@@ -58,6 +59,11 @@ void ConfigureMvc(WebApplicationBuilder builder)
         .ConfigureApiBehaviorOptions(options =>
         {
             options.SuppressModelStateInvalidFilter = true;
+        })
+        .AddJsonOptions(x =>
+        {
+            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
         });
 }
 
